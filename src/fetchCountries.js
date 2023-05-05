3694452328b2f029d9bcff5c0dc32c617cbc27c7
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-05-05 23:07:38 +0200

    “fix”

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,26 @@
-export { fetchCountries };
+// export { fetchCountries };
 
-const BASE_URL = 'https://restcountries.com/v3.1';
-const FILTER_RESPONSE = '?fields=name,capital,population,flags,languages';
+// const BASE_URL = 'https://restcountries.com/v3.1';
+// const FILTER_RESPONSE = '?fields=name,capital,population,flags,languages';
 
-function fetchCountries(name) {
-  return fetch(`${BASE_URL}/name/${name}${FILTER_RESPONSE}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
+// function fetchCountries(name) {
+//   return fetch(`${BASE_URL}/name/${name}${FILTER_RESPONSE}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.statusText);
+//     }
+//     return response.json();
+//   });
+// }
+
+
+export function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.status);
     }
-    return response.json();
+
+    return resp.json();
   });
 }
